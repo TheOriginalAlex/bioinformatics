40554150596f509cbf32c6f4a7575d4c2c2e67c1
Alex Johnson <hello@alex-johnson.net>
2019-02-21 20:28:34 -0800

    Make clump finder more efficient
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

--- modules/clumpfinding.py
+++ modules/clumpfinding.py
@@ -7,6 +7,9 @@ def word_frequencies(genome, k):
             patterns[pattern] = 1
         else:
             patterns[pattern] += 1
+    return patterns
+
+def word_counts(patterns):
     counts = {}
     for ptr, count in patterns.items():
         if count not in counts:
@@ -16,15 +19,26 @@ def word_frequencies(genome, k):
     return counts
 
 def ClumpFinding(genome, k, L, t):
-    patterns = {}
-    iterLen = len(genome)-L+1
     clumps = []
-    for i in range(0, iterLen):
-        clumpWin = genome[i:i+L]
-        counts = word_frequencies(clumpWin, k)
-        maxCount = max(counts)
-        if maxCount >= t:
-            clumps = clumps + counts[maxCount]
-            clumps = list(set(clumps))
-    return clumps
+    clumpWin = genome[0:L]
+    patterns = word_frequencies(clumpWin, k)
+    counts = word_counts(patterns)
+    maxCount = max(counts)
+    if maxCount >= t:
+        clumps = counts[maxCount]
+    iterLen = len(genome)-L+1
+    for i in range(1, iterLen):
+        ptr = genome[i-1:i-1+k]
+        if ptr in patterns:
+            patterns[ptr] -= 1
+        else:
+            patterns[ptr] = -1
+        ptr = genome[i+L-k:i+L]
+        if ptr in patterns:
+            patterns[ptr] += 1
+        else:
+            patterns[ptr] = 1
+        if patterns[ptr] >= t:
+            clumps.append(ptr)
+    return set(clumps)
 
